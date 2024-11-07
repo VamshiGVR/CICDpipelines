fa6b5f0f6e6654ba8a66117d9ff01156b6e24d8d
Vamshimittu1 <gunvamshidharreddy@gmail.com>
2024-11-07 13:52:58 -0500

    pytest

---
+++ tests/test_my_func.py
@@ -0,0 +1,26 @@
+import pytest
+import source.main as test_main
+
+
+#test on numbers
+def test_add():
+    result = test_main.add(3,2)
+    assert result == 5
+
+#def test_divide():
+    #reult = test_main.divide(1,0)
+    #assert reult == 2
+
+#def test_divide_0():
+    #with pytest.raises(ZeroDivisionError):
+        #test_main.divide(1,0)
+
+
+def test_divide_V():
+    with pytest.raises(ValueError):
+        test_main.divide(1,0)
+ 
+#Test on Strings 
+def test_add_strings():
+    result = test_main.add("I like ","Burger")
+    assert result == "I like Burger"
